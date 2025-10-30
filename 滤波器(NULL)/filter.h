783e7a01854ae5b02c46fe5da6e91c8e574fd3ee
星必尘Sguan <148698322+Sguan-ZhouQing@users.noreply.github.com>
2025-10-30 21:11:44 +0800

    Add files via upload

---
+++ 滤波器(NULL)/filter.h
@@ -0,0 +1,9 @@
+#ifndef __FILTER_H
+#define __FILTER_H
+
+float low_pass_filter(float input, float last_output, float alpha);
+float kalman_filter_std(float input, float r, float q);
+float kalman_filter_dir(float input, float r, float q);
+
+
+#endif // FILTER_H
